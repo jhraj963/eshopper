f744b75150b4c1c5a018af9e5827c7c1a69dad06
jhraj963 <you@example.com>
2024-09-14 19:16:02 +0600

    icon update

--- src/layouts/include/footer.js
+++ src/layouts/include/footer.js
@@ -152,8 +152,8 @@ function Footer() {
             <div className="footer-bottom">
                 <div className="container">
                     <div className="row">
-                        <p className="pull-left">Copyright © 2013 E-SHOPPER Inc. All rights reserved.</p>
-                        <p className="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p>
+                        <p className="pull-left">Copyright © 2024 E-SHOPPER Inc. All rights reserved.</p>
+                        <p className="pull-right">Designed by <span><a target="_blank" href="#">JHR</a></span></p>
                     </div>
                 </div>
             </div>
