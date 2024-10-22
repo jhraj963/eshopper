dbe164d239c9e9ed2d0956f1abea47d9c3d340c4
jhraj963 <jhraj963@gmail.com>
2024-10-22 13:17:17 +0600

    login

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -34,26 +34,29 @@ function Register() {
                 <div className="row">
                     <div className="col-12">
                         <div className="form-group">
-                            <label htmlFor="name_en">First Name</label>
-                            <input type="text" id="name_en" className="form-control" name="name_en" onChange={handleChange} />
+                            <label htmlFor="name">First Name</label>
+                            <input type="text" id="name" className="form-control" name="name" onChange={handleChange} />
                         </div>
                     </div>
+
                     <div className="col-12">
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
                             <input type="email" id="email" className="form-control" name="email" onChange={handleChange} />
                         </div>
                     </div>
-                    <div className="col-12">
-                        <div className="form-group">
-                            <label htmlFor="contact_no_en">Contact Number</label>
-                            <input type="text" id="contact_no_en" className="form-control" name="contact_no_en" onChange={handleChange} />
-                        </div>
-                    </div>
+
                     <div className="col-12">
                         <div className="form-group">
                             <label htmlFor="password">Password</label>
                             <input type="text" id="password" className="form-control" name="password" onChange={handleChange} />
+                        </div>
+                    </div>
+                    
+                    <div className="col-12">
+                        <div className="form-group">
+                            <label htmlFor="c_password">Confirm Password</label>
+                            <input type="text" id="c_password" className="form-control" name="c_password" onChange={handleChange} />
                         </div>
                     </div>
 
